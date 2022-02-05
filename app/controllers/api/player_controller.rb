5f4ac83f838df8a77d849adbe3e78be206aaccb7
Subin Cho <78716708+subinc1633@users.noreply.github.com>
2022-02-05 16:36:55 -0500

    set up rails backend

--- app/controllers/api/player_controller.rb
+++ app/controllers/api/player_controller.rb
@@ -1,3 +1,21 @@
 class PlayerController < ApplicationController
-    
+    def index
+        @players = Player.all
+        render 'api/players/index'
+    end
+
+    def create
+        @player = Player.new(player_params)
+        if @player.save
+            render 'api/players/index'
+        else
+            render json: @player.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def player_params
+        params.require(:player).permit(:name, :score)
+    end
 end
---
+++ frontend/store/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== "production") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+const configureStore = (preloadedState = {}) =>
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
+
+export default configureStore;
