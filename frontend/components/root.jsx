68baf95e877b6b81fd659c9ec679d77ca9988585
Subin Cho <78716708+subinc1633@users.noreply.github.com>
2022-02-09 12:06:18 -0500

    edit package.json

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import App from './app'
 
 const Root = ({ store }) => (
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <App />
     </Provider>
 );
 
